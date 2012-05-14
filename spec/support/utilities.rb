82488262f36483f28213158ab3957537c68e1e22
Reinaldo Oliveira <reinaldooli@gmail.com>
2012-05-14 11:19:53 -0300

    Improving Tests

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,5 +1,1 @@
-def full_title(page_title)
-  base_title = "Ruby on Rails Tutorial Sample App"
-  return base_title if page_title.empty?
-  return "#{base_title} | #{page_title}"
-end
+include ApplicationHelper
